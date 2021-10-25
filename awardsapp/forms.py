aacb3edd55244aa5ee7b7dbb96bb748d07e8d158
ObadiaH123 <kiprotichbernard780@gmail.com>
2021-10-25 16:50:29 +0300

    adding the forms

--- awardsapp/forms.py
+++ awardsapp/forms.py
@@ -22,7 +22,7 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'bio']   
+        fields = ['image', 'Bio']   
 class ProjectForm(forms.ModelForm):
 
     class Meta:
